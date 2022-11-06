ef087f48c8e53e8775f3208b6a48e0e1bc36cb44
Jakob A <jdanders1999@gmail.com>
2022-11-06 18:50:57 -0500

    Added number array and added more special characters to symbol array

--- Develop/script.js
+++ Develop/script.js
@@ -17,4 +17,5 @@ generateBtn.addEventListener("click", writePassword);
 var capitalLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 var lowercaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 // Only using most common symbols
-var symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "."]
+var symbols = ["!", "@", "#", "$", "%", "^", "&", "*", ".", "?", "_", "-"]
+var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
