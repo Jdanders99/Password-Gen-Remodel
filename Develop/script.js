62bbb213f4a272a1524e3aedf2c4c1cea9ff3ab8
Jakob A <jdanders1999@gmail.com>
2022-11-06 19:15:08 -0500

    Added variable/object for user inputs

--- Develop/script.js
+++ Develop/script.js
@@ -1,18 +1,3 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
 // Arrays with captial and lowercase letters
 var capitalLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 var lowercaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
@@ -52,4 +37,28 @@ function passwordPrompt() {
     window.alert("Must have one or more character types");
     return;
   }
+
+  var userInput = {
+    length: length,
+    lowerCase: lowerCase,
+    upperCase: upperCase,
+    incNumbers: incNumbers,
+    incSymbols: incSymbols,
+  };
+  return userInput;
 }
+
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
