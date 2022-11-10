3021811b850690655e5cba7a12051a04cc484030
Jakob A <jdanders1999@gmail.com>
2022-11-10 17:17:10 -0500

    Added notes to code for reference if needed, making it easily readable

--- Develop/script.js
+++ Develop/script.js
@@ -5,7 +5,7 @@ var lowercaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "
 var symbols = ["!", "@", "#", "$", "%", "^", "&", "*", ".", "?", "_", "-"]
 var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
 
-// User prompt
+// Function to prompt the user when they first open web page, sets stage for generator
 function passwordPrompt() {
   var length = parseInt(
   window.prompt("How long would you like your password to be?"),
@@ -52,38 +52,49 @@ function passwordPrompt() {
   return userInput;
 }
 
-// Calling on random array
+// Calling on random array based on previous function selections
 function randomize(arr) {
   var Index = Math.floor(Math.random() * arr.length);
   var randomArray = arr[Index];
   return randomArray;
 }
 
+// Function to choose letters, numbers or symbols based on user choice, includes at least one at random from each selected
 function passwordGen() {
   var totalOptions = passwordPrompt();
   var results = [];
   var totalCharacters = [];
   var inputCharacters = [];
 
+  // If they did not select anything, return to the main generator
   if (!totalOptions) 
   return;
-
+  
+  // Checking if lowercase was selected, if so, adding those characters from array
   if (totalOptions.lowerCase) {
     totalCharacters = totalCharacters.concat(lowercaseLetters);
     inputCharacters.push(randomize(lowercaseLetters));
   }
+
+  // Checking if uppsercase was selected, if so, adding those characters from array 
   if (totalOptions.upperCase) {
     totalCharacters = totalCharacters.concat(capitalLetters);
     inputCharacters.push(randomize(capitalLetters));
   }
+
+  // Checking if numbers were selected, if so, adding those characters from array
   if (totalOptions.incNumbers) {
     totalCharacters = totalCharacters.concat(numbers);
     inputCharacters.push(randomize(numbers));
   }
+
+  // Checking if symbols were selected, if so, adding those characters from array
   if (totalOptions.incSymbols) {
     totalCharacters = totalCharacters.concat(symbols);
     inputCharacters.push(randomize(symbols));
   }
+
+  // Checking length of options and randomizing characters based on user selection
   for (var x = 0; x < totalOptions.length; x++) {
     var generated = randomize(totalCharacters);
     results.push(generated);
